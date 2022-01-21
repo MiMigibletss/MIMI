3f59bd07e4eb38dfd513d4b4748f353b945863e7
kyunhyun94 <ekfqlwcjswl@naver.com>
2022-01-21 09:52:52 +0900

    add

--- server1/blockCheck.js
+++ server1/blockCheck.js
@@ -16,7 +16,7 @@ function isValidBlockStructure(block) {
     typeof block.body === "object"
   );
 }
-
+//11
 function isValidNewBlock(newBlock, previousBlock) {
   if (isValidBlockStructure(newBlock) === false) {
     console.log("유효하지 않은 블록구조입니다.");
